c49a349708303a780a67c715d8978aff05b4a9c7
Aosong Feng <45991889+asFeng@users.noreply.github.com>
2024-03-12 03:48:30 -0400

    Update run_remove.sh

--- scripts/sdxl/run_remove.sh
+++ scripts/sdxl/run_remove.sh
@@ -1,15 +1,17 @@
 export IMAGE_NAME="example1"
-CUDA_VISIBLE_DEVICES=1 python main.py --name=$IMAGE_NAME \
+python main.py --name=$IMAGE_NAME \
     --dpm="sdxl" \
     --resolution=1024 \
     --num_tokens=5 \
     --load_edited_mask \
     --load_trained \
     --remove \
-    --seed=2023 \
+    --seed=0 \
     --num_sampling_step=20 \
-    --strength=0.3 \
-    --edge_thickness=30 \
+    --strength=0.4 \
+    --edge_thickness=20 \
     --guidance_scale=3 \
     --num_imgs=1 \
-    --tgt_index=0
+    --tgt_index=0 
+
+    # --load_edited_processed_mask
