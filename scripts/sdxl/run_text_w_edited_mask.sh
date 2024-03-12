c83876d44e03dbd07b59fb33d2eaca2a11d15e59
Aosong Feng <45991889+asFeng@users.noreply.github.com>
2024-03-12 03:23:16 -0400

    Update run_text_w_edited_mask.sh

--- scripts/sdxl/run_text_w_edited_mask.sh
+++ scripts/sdxl/run_text_w_edited_mask.sh
@@ -2,6 +2,7 @@ export IMAGE_NAME="example1"
 python main.py --name=$IMAGE_NAME \
     --dpm="sdxl" \
     --resolution=1024 \
+    --num_tokens=5 \
     --load_trained \
     --load_edited_mask \
     --text \
